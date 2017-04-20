4a8d9d3c3bf07fa287545c8c18a0117a31b36d31
meshiest <meek.mesh@gmail.com>
2017-04-19 20:28:17 -0400

    Greatly improve generation code, added automation

--- gentasks.rb
+++ gentasks.rb
@@ -31,7 +31,6 @@ $things = {
   "Sorcery" => ["Magic", "Stone"],
   "Wizard" => ["Magic", "Human"],
   "Demigod" => ["Wizard", "Energy", "Light"],
-  "Blood" => ["Human", "Dinosaur"],
   "Vampire" => ["Human", "Blood"],
   "Animal" => ["Human", "Beast"],
   "Wool" => ["Human", "Animal"],
@@ -41,6 +40,9 @@ $things = {
   "Tools" => ["Human", "Metal"],
   "Weapon" => ["Tools", "Metal"],
   "Sex" => ["Human", "Human"],
+  "Love" => ["Human", "Human", "Time"],
+  "Children" => ["Sex", "Love", "Time"],
+  "Abuse" => ["Alcohol", "Children"],
   "Hunter" => ["Human", "Weapon"],
   "Ninja" => ["Hunter", "Assassin"],
   "Ninjutsu" => ["Ninja", "Magic"],
@@ -83,6 +85,8 @@ $things = {
   "Wheat" => ["Field", "Seeds"],
   "Flour" => ["Wheat", "Stone"],
   "Dough" => ["Flour", "Water"],
+  "Sugar" => ["Field", "Light"],
+  "Cake" => ["Sugar", "Egg", "Milk", "Wheat"],
   "Bread" => ["Dough", "Fire"],
   "Beer" => ["Bread", "Alcohol"],
   "Reed" => ["Grass", "Swamp"],
@@ -99,7 +103,9 @@ $things = {
   "Glass" => ["Sand", "Fire"],
   "Tobacco" => ["Grass", "Fire"],
   "Weed" => ["Grass", "Fire"],
+  "Baked" => ["Weed", "Human"],
   "Cigarette" => ["Tobacco", "Paper"],
+  "Joint" => ["Weed", "Paper"],
   "Fertilizer" => ["Animal", "Grass"],
   "Shell" => ["Stone", "Plankton"],
   "Limestone" => ["Shell", "Stone"],
@@ -121,9 +127,13 @@ $things = {
   "Torch" => ["Fire", "Wood"],
   "Lamp" => ["Electricity", "Glass"],
   "Mirror" => ["Light", "Stone"],
+  "Ego" => ["Mirror", "Human"],
+  "Intellect" => ["Human", "Time"],
+  "Science" => ["Intellect", "Time"],
   "Lense" => ["Light", "Crystal"],
   "Telescope" => ["Lense", "Mirror"],
   "Solarpanel" => ["Glass", "Circuit"],
+  "Beaker" => ["Glass", "Science"],
   "Circuit" => ["Metal", "Electricity"],
   "Mechanism" => ["Electricity", "Metal"],
   "Processor" => ["Circuit", "Mechanism"],
@@ -134,7 +144,7 @@ $things = {
   "Space" => ["Rocket", "Human"],
   "Moon" => ["Space", "Flag"],
   "Flag" => ["Fabric", "Metal"],
-  "Rover" => ["Car", "Space"],
+  "Rover" => ["Car", "Space", "Moon"],
   "Pillage" => ["Flag", "Village", "Army"],
   "Conquer" => ["Pillage", "Blood"],
   "Blood" => ["Human", "Weapon"],
@@ -150,26 +160,90 @@ $things = {
   "Royalty" => ["Sex", "Money"],
   "Money" => ["Village", "Metal"],
   "Empire" => ["Conquer", "Colony", "King"],
+  "Drill" => ["Electricity", "Tools", "Mechanism"],
 }
 
+# Breaks a thing down into components
 def what_is thing
-  $things[thing] && $things[thing].map{|t| what_is(t)}.flatten || thing
+  $things[thing] && $things[thing].map{|t| what_is(t)}.flatten || [thing]
 end
 
-raw = $things.map{|k, v| what_is k}.flatten
-base = raw.uniq.map{|t| "{id: \"#{t.downcase}\", count: #{raw.count(t)}}"}
+# Takes an array and breaks the things down into components
+def what_are things
+  things.map{|k| what_is k}.flatten
+end
 
-tasks = $things.map { |k, v|
-  "  #{k.downcase}: new Task(\"#{k.downcase}\", \"#{k}\", -1, #{1000 + what_is(k).length * 1000}, [#{v.map{|t| "{id: \"#{t.downcase}\", count: 1}"}.join(", ")}]),"
-}.join("\n")
+$upgrades = {
+  "Miner" => {
+    cost: ["Drill", "Tools", "Human", "Energy"],
+    action: "Mine Stone",
+    uses: -1,
+    speed: 500,
+    input: what_is("Stone") * 10,
+    output: ["Stone"] * 10,
+  },
+  "Smelter" => {
+    cost: ["Mechanism", "Fire", "Tools", "Human"],
+    action: "Smelt Metal",
+    uses: -1,
+    speed: 500,
+    input: what_is("Metal") * 10,
+    output: ["Metal"] * 10,
+  },
+  "Breeder" => {
+    cost: ["Human", "Beaker", "Sex", "Intellect", "Tools"],
+    action: "Breed Human",
+    uses: -1,
+    speed: 500,
+    input: what_is("Human"),
+    output: ["Human"],
+  },
+}
 
-puts """
-// All available tasks
+# "Creation Name" => {
+#   cost: ["Ingredients", "Like", "Things"],
+#   action: "Action Name",
+#   uses: -1, # -1 for unlimited uses (but one time craft), > 1 for limited uses and unlimited crafts
+#   speed: 500, # Speed * Raw Ingredients = Duration
+#   input: ["Ingredient", "Ingredient"], # or what_is("Thing") * n
+#   output: ["Ingredient", "Ingredient"],
+# },
+
+# Lazy Function
+def stringify things
+  things.uniq.map { |t|
+    "{id: \"#{t.downcase}\", count: #{things.count(t)}}"
+  }.join(", ")
+end
+
+# Add all the upgrade roots as things
+$upgrades.each { |k, v|
+  $things[k] = v[:cost]
+}
+
+# The first task must be the starting task
+tasks = ["  things: new Task(\"things\", \"Create Things\", 1, 5000, [], 0, [#{stringify what_are $things.keys}]),"]
+
+# Create all the thing tasks
+tasks += $things.map { |k, v|
+  "  #{k.downcase}: new Task(\"#{k.downcase}\", \"#{k}\", #{$upgrades[k] ? (($upgrades[k][:uses] || -1) == -1 ? 1 : -1 ) : -1}, #{1000 + what_is(k).length * 1000}, [#{stringify v}]),"
+}
+
+# Create all the upgrade tasks
+tasks += $upgrades.map { |k, v|
+  speed = what_are(v[:input]).length * v[:speed]
+  "  action_#{k.downcase}: new Task(\"action_#{k.downcase}\", \"#{v[:action]}\", #{$upgrades[k][:uses] || -1}, #{speed}, [{id: \"#{k}\", count: 0}, #{stringify v[:input]}], 0, [{id: \"action_#{k.downcase}\", count: -1}, #{stringify v[:output]}]),"
+}
+
+# Join the tasks
+tasks = tasks.join("\n")
+
+# Print the code
+puts """// All available tasks
 let tasks = {
-  elements: new Task(\"elements\", \"Create Elements\", 1, 5000, [], 0, [#{base.join(", ")}]),
 #{tasks}
 };
 
 // Initial Tasks
-let initial = [tasks.elements];
+let initial = [tasks.things];
 """
